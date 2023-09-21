eb1ad42623b09bedffc5e02c4bbf4b81a170e491
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-09-21 19:10:06 +0400

    Near the top of main.go, just beneath the package declaration, I have imported the packages that I’ll need to support the code you’ve just written.

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,5 +1,12 @@
 package main
 
+// Importing Packages to support the Go code that will be written
+import (
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+)
+
 // album represents data about a record album.
 type album struct {
     ID     string  `json:"id"`

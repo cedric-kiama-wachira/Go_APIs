645611bea5109bb16c811108a2580414d967e90f
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-09-21 19:44:09 +0400

    Finally, I changed the main fucntion so that it includes a new call to router.GET, where the path is now /albums/:id.

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -50,6 +50,7 @@ type album struct {
 func main() {
     router := gin.Default()
     router.GET("/albums", getAlbums)
+    router.GET("/albums/:id", getAlbumByID)
     router.POST("/albums", postAlbums)
 
     router.Run("localhost:8080")

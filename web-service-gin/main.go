faccebd481bba0c19ae6bd03b83ac66fa0eccd57
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-09-21 19:23:18 +0400

    I have modified the file and added code to add albums data for the list of albums. This is written between the import statements and album struct section

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -7,6 +7,21 @@ import (
     "github.com/gin-gonic/gin"
 )
 
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+    var newAlbum album
+
+    // Call BindJSON to bind the received JSON to
+    // newAlbum.
+    if err := c.BindJSON(&newAlbum); err != nil {
+        return
+    }
+
+    // Add the new album to the slice.
+    albums = append(albums, newAlbum)
+    c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
 // album represents data about a record album.
 type album struct {
     ID     string  `json:"id"`
